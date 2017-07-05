b50ed8e52eda4367b6ac5bd3199284a4cb3896f3
tsubasamorisan <tsubasamori.eros@gmail.com>
2017-07-05 11:21:16 -0600

    fixed prod bundle

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,7 @@ const EXCLUDES = [/node_modules/];
 var plugins = [
     mainStylesExtract,
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('development')
-    })
+    new webpack.HotModuleReplacementPlugin()
 ];
 
 module.exports = {
