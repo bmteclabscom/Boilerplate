3c886ee6fa55ca146e657b11544c71610a555c43
tsubasamorisan <tsubasamori.eros@gmail.com>
2017-07-11 14:50:30 -0600

    forms using bootstrap error css

--- client/js/components/form-fields.js
+++ client/js/components/form-fields.js
@@ -4,21 +4,32 @@ import { Field } from 'redux-form';
 /**
  * Renders regular text tag field
  */
-export const renderTextField = ({ input, label, type, placeholder, className, options, meta: { touched, error }, children }) => (
-    <div className={`form-group ${className}`}>
-        <label className="control-label" hidden={!label}>{label}</label>
-        <input {...input} placeholder={placeholder} type="text" className="form-control"/>
-        {touched && error && <span className="form-group-error">{error}</span>}
-    </div>
-);
+export const renderTextField = ({ input, label, type, placeholder, className, options, meta: { touched, error }, children }) => {
+    const hasError = touched && error;
+    const errorCss = hasError ? 'has-danger' : '';
+    const errorFieldCss = hasError ? 'form-control-danger' : '';
+    return (
+        <div className={`form-group ${className} ${errorCss}`}>
+            <label className="form-control-label" hidden={!label}>{label}</label>
+            <input {...input} placeholder={placeholder} type="text" className={`form-control ${errorFieldCss}`}/>
+            {hasError && <div className="form-control-feedback">{error}</div>}
+        </div>
+    ); 
+}
+
 
 /**
  * Renders password text tag field
  */
-export const renderPasswordField = ({ input, label, type, placeholder, className, options, meta: { touched, error }, children }) => (
-    <div className={`form-group ${className}`}>
-        <label className="control-label" hidden={!label}>{label}</label>
-        <input {...input} placeholder={placeholder} type="password" className="form-control"/>
-        {touched && error && <span className="form-group-error">{error}</span>}
-    </div>
-);
+export const renderPasswordField = ({ input, label, type, placeholder, className, options, meta: { touched, error }, children }) => {
+    const hasError = touched && error;
+    const errorCss = hasError ? 'has-danger' : '';
+    const errorFieldCss = hasError ? 'form-control-danger' : '';
+    return (
+        <div className={`form-group ${className} ${errorCss}`}>
+            <label className="form-control-label" hidden={!label}>{label}</label>
+            <input {...input} placeholder={placeholder} type="password" className={`form-control ${errorFieldCss}`}/>
+            {hasError && <div className="form-control-feedback">{error}</div>}
+        </div>
+    ); 
+}
